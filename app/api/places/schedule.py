76502661e378fc54c84f792d8046e6e7fd2279b4
cmir22 <isc.cruzibarra@gmail.com>
2023-04-19 22:03:57 -0700

    working with create schedule

--- app/api/places/schedule.py
+++ app/api/places/schedule.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Header
 from database.collections import places_schedule_collection
 from database.db import database
 from helpers.exeptions import error_insert
-from database.models.places.places_model import Place
+from database.models.places.places_schedule_model import PlaceSchedule
 from helpers.responses import success_message
 from security.jwt_decode import decode_token
 
@@ -17,13 +17,21 @@ places_schedule_collection = database[places_schedule_collection]
 
 
 @router.post("/create")
-async def create_place_info(schedule: Place, Authorization: str = Header(None)):
-    print(decode_token(Authorization))
-    # try:
-    #     schedule_dict = dict(schedule)
-    #     document = places_schedule_collection.insert_one(schedule_dict)
-    # except Exception as exs:
-    #     raise error_insert(exs)
+async def create_place_info(schedule: PlaceSchedule, Authorization: str = Header(None)):
+    days = [0, 2, 3, 4, 5, 6, 7, 8]
+    try:
+        id_place = decode_token(Authorization)["id_place"]
+        schedule_dict = dict(schedule)["days"]
+        schedule_list = []
 
-    # return success_message(document.inserted_id)
+        for sch in schedule_dict:
+            schedule_list[str(sch["day_number"])] = sch
+
+        # print(sche)
+
+        # document = places_schedule_collection.insert_many(schedule_dict)
+    except Exception as exs:
+        raise error_insert(exs)
+
     return {}
+    # return success_message(document.inserted_id)
--- app/database/models/places/places_schedule_model.py
+++ app/database/models/places/places_schedule_model.py
@@ -1,18 +1,26 @@
 
 ### User model ###
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel  # , Field
 # from typing import Optional
-from bson import ObjectId
-from datetime import datetime
+# from bson import ObjectId
+# from datetime import datetime
 from datetime import time
+
+
+class Schedule(BaseModel):
+    _id_place: str
+    day_number: int
+    open_time: time
+    close_time: time
 
 
 # Long vertion
 class PlaceSchedule(BaseModel):
-    _id: ObjectId = Field(alias='_id')
-    _id_place: str
-    day: str
-    open_time: time
-    close_time: time
-    last_edit_date = datetime.now().today()
+    days: list[Schedule]
+    # _id: ObjectId = Field(alias='_id')
+    # _id_place: str
+    # day_number: int
+    # open_time: time
+    # close_time: time
+    # last_edit_date = datetime.now().today()
